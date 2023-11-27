2a5a91ec97e906e046cf118cee0333e90e6b9ee4
rytale1 <rxiaschool24@gmail.com>
2023-11-27 15:40:04 -0800

    Ryan's test commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>Hellfo</p>
+                <p>I love men</p>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
