d2b196f90246fe8a684928542cb8d3e7ad70924b
Snorelaxrules <leongtalus@gmail.com>
2023-11-27 18:44:16 -0800

    hola

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>Hellfo</p>
+                <p>HOLA</p>
                 <a
                     className="App-link"
                     href="https://reactjs.org"
