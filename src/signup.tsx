563ff09a8c582e2d94a30f7b0071904b674f4745
rytale1 <rxiaschool24@gmail.com>
2024-01-13 16:28:13 -0800

    added login and signup pages

---
+++ src/signup.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+
+function signup () {
+    return(
+        <>
+            <p>
+               Sign Up Here 
+            </p>
+        </>
+    );
+}
+
+export default signup
